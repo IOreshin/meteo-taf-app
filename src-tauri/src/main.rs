a9ec086de70a776645c8d630e9a260985975d4fa
IOreshin <joreshin@gmail.com>
2025-11-22 22:09:19 +0300

    компиляция

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,5 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
+
 
 use tauri::{command};
 use std::fs;
